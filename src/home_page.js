23e0223a566a4e5bd803cabe7aeb0de1e5e1fbef
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-06-27 15:26:23 +0530

    Added profession text

--- src/home_page.js
+++ src/home_page.js
@@ -7,6 +7,11 @@ const HomePage = () => {
             <div className="name">
                 Hello!!! I'm Rohit Bharat Bhandwalkar
             </div>
+            <div className="profession">
+                <div id = "first_text"> I'm A &#60;</div>
+                <div id = "second_text">Developer</div>
+                <div id = "third_text">/&#62;</div>
+            </div>
         </div>
     );
 }
