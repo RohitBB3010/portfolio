e75b6e453805bc1e2f5d391582c8071de1825ffa
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-06-27 16:44:44 +0530

    Added animation for roles

--- src/home_page.js
+++ src/home_page.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
 import './home_page.css';
 
 const HomePage = () => {
+
+    const roles = ['Full Stack', 'Mobile App'];
+    const [roleIndex, setRoleIndex] = useState(0);
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setRoleIndex(roleIndex === 0 ? 1 : 0);
+        }, 2000);
+
+        return() => {
+            clearInterval(interval);
+        }
+    }, );
 
     return(
         <div className="home_page">
@@ -11,6 +25,10 @@ const HomePage = () => {
                 <div id = "first_text"> I'm A &#60;</div>
                 <div id = "second_text">Developer</div>
                 <div id = "third_text">/&#62;</div>
+            </div>
+            <div className="role_animation">
+                <div id="role">{roles[roleIndex]} </div>
+                <div id="developer">Developer </div>
             </div>
         </div>
     );
