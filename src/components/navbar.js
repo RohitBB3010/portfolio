cf7fb30076e71ac5b089bb49a9484cabf8dcbd20
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-06-27 12:21:22 +0530

    Improved navbar placement

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,11 +5,11 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <ul className="navbar_list">
-                <li className="navbar_items"> Home </li>
-                <li className="navbar_items"> Skills </li>
-                <li className="navbar_items" id="navbar_heading"> Portfolio</li>
-                <li className="navbar_items"> Experience </li>
-                <li className="navbar_items"> Contact </li>
+                <li className="navbar_items"> <a href = "#!"> Home </a> </li>
+                <li className="navbar_items"> <a href = "#!"> Skills </a> </li>
+                <li className="navbar_items" id="navbar_heading"> <a href = "#!"> Portfolio </a> </li>
+                <li className="navbar_items"> <a href = "#!"> Experience </a> </li>
+                <li className="navbar_items"> <a href = "#!"> Contact </a> </li>
             </ul>
         </div>
     );
