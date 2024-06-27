0813ba4b950f30aac50ead9c6c0edf4d11988849
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-06-27 13:00:04 +0530

    Added intro text:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar';
+import HomePage from './home_page';
 
 function App() {
   return (
     <div className="App">
       <Navbar> </Navbar>
+      <HomePage> </HomePage>
     </div>
   );
 }
---
+++ src/home_page.js
@@ -0,0 +1,14 @@
+import './home_page.css';
+
+const HomePage = () => {
+
+    return(
+        <div className="home_page">
+            <div className="intro">
+                Hello!! I'm Rohit Bharat Bhandwalkar
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
