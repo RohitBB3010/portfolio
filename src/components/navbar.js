efb13b781137e4da6a831d5533f07ff7b82020fc
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-06-27 12:28:17 +0530

    Added a tag styles navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,11 +5,11 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <ul className="navbar_list">
-                <li className="navbar_items"> <a href = "#!"> Home </a> </li>
-                <li className="navbar_items"> <a href = "#!"> Skills </a> </li>
-                <li className="navbar_items" id="navbar_heading"> <a href = "#!"> Portfolio </a> </li>
-                <li className="navbar_items"> <a href = "#!"> Experience </a> </li>
-                <li className="navbar_items"> <a href = "#!"> Contact </a> </li>
+                <li> <a className="navbar_items" href = "#!"> Home </a> </li>
+                <li> <a className="navbar_items"  href = "#!"> Skills </a> </li>
+                <li > <a className="navbar_items" href = "#!" id="navbar_heading"> Portfolio </a> </li>
+                <li> <a className="navbar_items" href = "#!"> Experience </a> </li>
+                <li> <a className="navbar_items"  href = "#!"> Contact </a> </li>
             </ul>
         </div>
     );
