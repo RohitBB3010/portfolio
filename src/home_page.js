958a185dfe5900e173adae4d0096344804fd10ab
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-06-28 00:40:10 +0530

    completed home page

--- src/home_page.js
+++ src/home_page.js
@@ -33,6 +33,7 @@ const HomePage = () => {
             <div className="info"> 
             Welcome to my developer portfolio! Dive into a world of innovative projects, cutting-edge skills, and a passion for creating exceptional digital experiences.
             </div>
+            <a href = "https://drive.google.com/file/d/1Z86am44XEwJF6zeXfZsWZ4TR_wd5OK0F/view" id = "resume" target = "_blank"> Resume </a>
             </div>
             <img src="/ellipse.png" alt = ""/>  
         </div>
