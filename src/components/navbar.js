156758619a4c610a8edb556e4365bb95fb6b1eee
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-06-28 12:18:56 +0530

    navbar changed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,18 +5,16 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <ul className="navbar_list">
-                {/* <li> <a className="navbar_items" href = "#!"> Home </a> </li>
-                <li> <a className="navbar_items"  href = "#skills"> Skills </a> </li>
-                <li > <a className="navbar_items" href = "#!" id="navbar_heading"> Portfolio </a> </li>
-                <li> <a className="navbar_items" href = "#!"> Experience </a> </li>
-                <li> <a className="navbar_items"  href = "#!"> Contact </a> </li> */}
+            <div className="navbar_left">
+                &#123; R &#125;
+            </div>
+            <div className="navbar_list">                
                 <Link to = "/" className="navbar_items"> Home </Link>
                 <Link to = "/skills" className="navbar_items"> Skills </Link>
                 <Link className="navbar_items" id = "navbar_heading"> Portfolio </Link>
                 <Link to = "/experience" className="navbar_items"> Experience </Link>
                 <Link to = "/contact" className="navbar_items"> Contact </Link>
-            </ul>
+            </div>
         </div>
     );
     
